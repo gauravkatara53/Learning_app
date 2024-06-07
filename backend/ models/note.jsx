37e85cde129e37d7ac495e641ecbfeddb9781a4d
gauravkatara53 <gauravkatara53@gmail.com>
2024-06-07 09:09:27 +0530

    change

--- backend/ models/note.jsx
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const noteSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    content: { type: String, required: true },
-    pdfPath: { type: String }
-});
-
-module.exports = mongoose.model('Note', noteSchema);
