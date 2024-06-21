16f86fc0cdbb75f5d8cd3f0bcdc44238af0e5e9b
gauravkatara53 <gauravkatara53@gmail.com>
2024-06-21 13:11:09 +0530

    backend

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -29,31 +29,6 @@ function Navbar() {
             </span>
           </a>
           <div className="flex md:order-2 items-center space-x-4">
-            <div className="relative hidden md:block">
-              <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                <svg
-                  className="w-4 h-4 text-gray-500"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 20 20"
-                >
-                  <path
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                  />
-                </svg>
-              </div>
-              <input
-                type="text"
-                id="search-navbar"
-                className="block w-40 p-2 pl-10 text-sm text-gray-800 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
-                placeholder="Search..."
-              />
-            </div>
             <button
               type="button"
               className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-300"
@@ -180,7 +155,7 @@ function Navbar() {
                 <Link
                   to="/community"
                   className={`block py-2 px-3 md:py-2 md:px-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-500 md:p-0 ${
-                    location.pathname === "/group"
+                    location.pathname === "/community"
                       ? "md:text-blue-500 bg-gray-100"
                       : ""
                   }`}
